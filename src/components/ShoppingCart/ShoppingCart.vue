b323503e2697d397ff8164f574f0d9f2bffb9eb9
songshuzhong <sshuzhong@outlook,com>
2023-11-14 14:47:57 +0800

    update

--- src/components/ShoppingCart/ShoppingCart.vue
+++ src/components/ShoppingCart/ShoppingCart.vue
@@ -78,7 +78,7 @@ export default defineComponent({
     };
     const onLinkageFormatting = (target, data) => {
       if (target === props.name) {
-        const hasOne = state.data[props.name].filter(i => i[props.primaryKey] = data[props.primaryKey]);
+        const hasOne = state.data[props.name].filter(i => i[props.primaryKey] === data[props.primaryKey]);
         if (hasOne.length) {
           hasOne[0].count = (hasOne[0].count || 0) + 1;
         } else {
